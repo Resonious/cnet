068ef40bd7c064c391da5146110c47c163b38f6e
Nigel Baillie <metreckk@gmail.com>
2015-01-25 20:24:28 -0500

    Mess around with better conditional compilation

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,18 @@ use std::io::net::ip::{Ipv4Addr, SocketAddr};
 use std::thread::Thread;
 use std::mem::transmute;
 
-struct Header {
-    op: i8,
+
+struct Vec2 {
+    x: f32, y: f32
 }
+
+// fn possible_use() {
+//     let game = create_game(
+//         Ipv4Addr(167, 114, 96, 204), 34569,
+//         "TheGame"
+//     );
+// }
+
 
 fn join() {
     let local_addr = SocketAddr { ip: Ipv4Addr(0, 0, 0, 0), port: 34568 };
@@ -15,7 +24,7 @@ fn join() {
     };
 
     let host_addr = SocketAddr { ip: Ipv4Addr(167, 114, 96, 204), port: 34567 };
-    let mut buf = [Header { op: 8 }];
+    let mut buf = [8u8];
     unsafe { socket.send_to(transmute(buf.as_slice()), host_addr); }
 
     println!("Sent data, now waiting for response");
@@ -56,8 +65,22 @@ fn host() {
 }
 
 #[test]
+// Uses should_fail so that we can get output.
 #[should_fail]
-fn it_works() {
-    host();
+#[cfg(feature = "client")]
+fn joining_works() {
     join();
+}
+
+#[test]
+#[should_fail]
+#[cfg(feature = "server")]
+fn hosting_works() {
+    host();
+}
+
+#[test]
+#[and(not(cfg(feature = "server")), not(cfg(feature = "client")))]
+fn plz_specify_client_or_server() {
+    panic!("Please specify --cfg server or --cfg client");
 }
