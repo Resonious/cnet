1c937443c2134a37b99110e05ce38ab7566cc87a
root <root@vps32960.vps.ovh.ca>
2015-03-28 19:03:18 -0400

    okay actually functions now

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-#![feature(ip_addr)]
+#![feature(ip_addr, convert)]
 
 use std::net::UdpSocket;
-use std::net::{SocketAddr, IpAddr};
+use std::net::{SocketAddr, IpAddr, Ipv4Addr};
 use std::str::FromStr;
 use std::string::String;
 use std::borrow::Borrow;
+use std::convert::AsRef;
 
 fn main() {
     let mut stdin = std::io::stdin();
@@ -13,14 +14,16 @@ fn main() {
     let mut ipstr = String::new();
     stdin.read_line(&mut ipstr);
 
-    let ip_addr = IpAddr::from_str(ipstr.borrow()).unwrap();
-
-    let addr = SocketAddr::new(ip_addr, 34561);
+    ipstr.pop();
+    let ip_addr = Ipv4Addr::from_str(ipstr.as_ref()).unwrap();
+    let addr = SocketAddr::new(IpAddr::V4(ip_addr), 34561);
 
     let mut socket = match UdpSocket::bind(addr) {
         Ok(s) => s,
         Err(e) => panic!("Failed to bind socket! {}")
     };
+
+    println!("I survived!");
 
     /*
     let mut buf = [0u8; 256];
