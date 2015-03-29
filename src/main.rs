1894879426699483bc9ffd91cc7c194cfff6cc98
Resonious <metreckk@gmail.com>
2015-03-29 10:49:29 -0400

    separate so maybe I can run tests

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,34 @@ use std::string::String;
 use std::borrow::Borrow;
 use std::convert::AsRef;
 
-fn main() {
-    let mut stdin = std::io::stdin();
+fn start_server(ip: &str) {
+  let ip_addr = Ipv4Addr::from_str(ip).unwrap();
+  let addr = SocketAddr::new(IpAddr::V4(ip_addr), 34561);
 
-    println!("Enter ip address!");
-    let mut ipstr = String::new();
-    stdin.read_line(&mut ipstr);
+  let mut socket = match UdpSocket::bind(addr) {
+    Ok(s) => s,
+    Err(e) => panic!("Failed to bind socket! {}")
+  };
 
-    ipstr.pop();
-    let ip_addr = Ipv4Addr::from_str(ipstr.as_ref()).unwrap();
-    let addr = SocketAddr::new(IpAddr::V4(ip_addr), 34561);
+  println!("I survived! (now listening??)");
 
-    let mut socket = match UdpSocket::bind(addr) {
-        Ok(s) => s,
-        Err(e) => panic!("Failed to bind socket! {}")
-    };
-
-    println!("I survived! (now listening??)");
-
-    let mut buf = [0u8; 256];
-    match socket.recv_from(&mut buf) {
-        Ok((len, src_addr)) => {
-            println!("Fuckin' got it!!!!!!!!!!!!!!!!!!!!!!!");
-        }
-
-        Err(e) => println!("Failed to receive :(! error: {}", e)
+  let mut buf = [0u8; 256];
+  match socket.recv_from(&mut buf) {
+    Ok((len, src_addr)) => {
+      println!("Fuckin' got it!!!!!!!!!!!!!!!!!!!!!!!");
     }
+
+    Err(e) => println!("Failed to receive :(! error: {}", e)
+  }
+}
+
+fn main() {
+  let mut stdin = std::io::stdin();
+
+  println!("Enter ip address!");
+  let mut ipstr = String::new();
+  stdin.read_line(&mut ipstr);
+
+  ipstr.pop();
+  start_server(ipstr.as_ref());
 }
