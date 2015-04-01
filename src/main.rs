ccba0c1f9fd3c2d5495b7c595c2c721dbbf62cc0
root <root@vps32960.vps.ovh.ca>
2015-04-01 12:10:50 -0400

    added ping test and bogus ping procedure

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,15 @@ fn start_server(ip: &str) {
     match socket.recv_from(&mut buf) {
       Ok((len, src_addr)) => {
         println!("Fuckin' got it!!!!!!!!!!!!!!!!!!!!!!!");
+        if len == 1 && buf[0] == 1 {
+          // ping!
+          unsafe {
+            let mut ms = transmute::<_, *mut i32>(&buf[0]);
+            // NOTE random value for now...
+            *ms = 15;
+            socket.send_to(buf.as_ref(), src_addr).unwrap();
+          }
+        }
       }
 
       Err(e) => println!("Failed to receive :(! error: {}", e)
@@ -57,11 +66,32 @@ fn start_test_server() {
     start_server("127.0.0.1");
   });
 }
+
 #[test]
 fn server_can_receive_packet() {
   server_test!((socket, host_addr) {
     let mut buf = [0u8; 256];
     buf[0] = 90;
     unsafe { socket.send_to(transmute(buf.as_ref()), host_addr).unwrap(); };
+  });
+}
+
+#[test]
+fn server_can_be_pinged() {
+  server_test!((socket, host_addr) {
+    unsafe {
+      let mut buf = [1u8];
+      socket.send_to(transmute(buf.as_ref()), host_addr).unwrap();
+
+      let mut in_buf = [0u8, 256];
+      match socket.recv_from(&mut in_buf) {
+        Ok((len, src_addr)) => {
+          let ms = *transmute::<_, *const i32>(&in_buf[0]);
+          println!("Got a ping of {} ms!", ms);
+        }
+
+        Err(e) => panic!("Got error {} when trying to ping!", e)
+      }
+    }
   });
 }
