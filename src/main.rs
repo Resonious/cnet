2de7500567ef40658db5ed009120aea8d884afcb
Resonious <metreckk@gmail.com>
2015-04-04 00:05:07 -0400

    slowly but surely, game creation

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,47 @@ use std::str::FromStr;
 use std::string::String;
 use std::convert::AsRef;
 use std::thread;
-// use std::mem::transmute;
+use std::mem::{zeroed, transmute};
+use std::ptr;
 use std::sync::mpsc;
 use std::sync::mpsc::TryRecvError::{Empty, Disconnected};
 use std::time::duration::Duration;
+use std::time;
 
 #[macro_use]
 mod testing;
+
+#[derive(Clone, Copy)]
+struct Player {
+  // assigned, given
+  id: (i8, i8),
+  last_received_packet_at: u64, // In nanoseconds
+}
+
+#[derive(Clone, Copy)]
+struct Game<'a> {
+  id: u8,
+  name: &'a str,
+  players: [Player; 10]
+}
+
+impl<'a> Game<'a> {
+  fn new(id: u8, name: &'a str) -> Game<'a> {
+    Game {
+      id: id,
+      name: name,
+      players: unsafe { zeroed() }
+    }
+  }
+}
+
+mod in_op {
+  pub const NEW_GAME: u8 = 2;
+}
+mod out_op {
+  pub const GAME_CREATED: u8 = 2;
+}
+const MAX_GAMES: usize = 20;
 
 fn start_server(ip: &str) {
   let ip_addr = Ipv4Addr::from_str(ip).unwrap();
@@ -26,18 +60,57 @@ fn start_server(ip: &str) {
   println!("I survived! (now listening??)");
 
   let mut buf = [0u8; 256];
+  let mut games = [None; MAX_GAMES];
   loop {
     match socket.recv_from(&mut buf) {
+      Err(e) => println!("Failed to receive :(! error: {}", e),
+
       Ok((len, src_addr)) => {
+
+        // ======================= BASIC PING =================
         if len == 1 && buf[0] == 1 {
-          // ping!
           println!("ping!!!");
           let response = [1u8];
           socket.send_to(response.as_ref(), src_addr).unwrap();
+          continue;
+        }
+
+        // ==================== OTHER OPERATIONS ==============
+        match buf[0] {
+          in_op::NEW_GAME => {
+            let mut index = -1;
+            // TODO grab the requested game name, then check if that name exists.
+            // (using this loop)
+            for i in 0..MAX_GAMES {
+              match games[i] {
+                Some(_) => continue,
+                None => {
+                  index = i;
+                  break;
+                }
+              }
+            }
+
+            // TODO inform the client that we have no more room instead of whining.
+            if index < 0 { println!("Ran out of room for games!!!!!!!!!"); continue; }
+
+            // TODO actually read packet for name lol.
+            games[index] = Some(Game::new(index as u8, "who cares"));
+
+            // TODO accept a packet id so that the client can know what responses mean.
+            // (maybe)
+            let response = [out_op::GAME_CREATED];
+            socket.send_to(response.as_ref(), src_addr).unwrap();
+
+            println!("MADE GAME");
+          }
+
+          _ => {
+            // TODO send back an UNKNOWN_OP op
+            println!("Unknown opcode {} in a {}-byte packet from {}", buf[0], len, src_addr);
+          }
         }
       }
-
-      Err(e) => println!("Failed to receive :(! error: {}", e)
     }
     println!("going again!");
   }
@@ -55,6 +128,8 @@ fn main() {
   start_server(ipstr.as_ref());
 }
 
+
+// =========================================== TESTS ========================================
 
 // NOTE This MUST be run before any server tests!
 #[test]
@@ -91,5 +166,30 @@ fn server_can_be_pinged() {
       }
     });
     println!("Ping took {} ms!", ms.num_milliseconds());
+  });
+}
+
+#[test]
+fn server_can_create_a_game() {
+  server_test!((socket, host_addr) {
+    let mut buf = [0u8; 128];
+    buf[0] = in_op::NEW_GAME;
+    let name = b"Stupid game";
+
+    buf[1] = name.len() as u8;
+    unsafe {
+      ptr::copy(&name[0], &mut buf[2], name.len());
+    }
+
+    socket.send_to(buf.as_ref(), host_addr).unwrap();
+
+    let mut in_buf = [0u8; 256];
+    match socket.recv_from(&mut in_buf) {
+      Ok((len, _src_addr)) => {
+        assert_eq!(in_buf[0], out_op::GAME_CREATED);
+      }
+
+      Err(e) => panic!("Got error {} when trying to create game!", e)
+    }
   });
 }
